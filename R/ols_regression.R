d35f738f739e6fc0734caf4865b6efdfdf702d1c
Mike Kearney <kearneymw@missouri.edu>
2018-04-30 14:49:40 -0500

    more fit statistics

--- R/ols_regression.R
+++ R/ols_regression.R
@@ -13,40 +13,34 @@
 #'
 #' @export
 ols_regression <- function(data, model, ...) {
+  f <- as.character(rlang::enquo(model))
   m <- lm(model, data = data)
   fit_statistics <- ols_fit(m)
   model_estimates <- broom::tidy(m)
   model_estimates <- add_stars(model_estimates)
-  #preds <- get_preds(model)
-  #response <- get_response(model)
-  #r2s <- get_r2(data, response, preds)
-  #model_estimates$r2_chg <- NA_real_
-  #terms <- paste0("^", preds)
-  #pt <- character(length(terms))
-  #for (i in seq_len(length(terms))) {
-  #  pt[i] <- gsub(terms[i], "", model_estimates$term[-1][i])
-  #  pt[i] <- paste0(pt[i], "$")
-  #  pt[i] <- gsub(pt[i], "", model_estimates$term[-1][i])
-  #}
-  #model_estimates$r2_chg[-1] <- r2s[match(pt, preds)]
+  cat(paste0("## Model call:\n", f[2], "\n"), fill = TRUE)
+  cat("## Model estimates:", fill = TRUE)
   list(fit = fit_statistics,
     coef = model_estimates)
 }
 
 ols_fit <- function(m) {
   s <- summary(m)
-  s$fstatistic
-  anova(m)
   a <- unname(s$fstatistic)
-  anm <- paste0("F(", a[2], ", ", a[3], ")")
   fp <- pf(s$fstatistic[1L],
     s$fstatistic[2L],
     s$fstatistic[3L],
     lower.tail = FALSE)
+  ll <- logLik(m)
+  aic <- AIC(m)
+  bic <- BIC(m)
   df <- data.frame(
-    statistic = c(anm, "R^2", "Adj R^2", "RMSE"),
-    estimate = c(a[1], s$r.squared, s$adj.r.squared, s$sigma),
-    p.value = c(fp, NA_real_, NA_real_, NA_real_),
+    fit_statistic = c("F", "R^2", "Adj R^2", "RMSE", "Log-likelihood", "AIC", "BIC"),
+    n = nobs(m),
+    df = c(a[2], NA_integer_, NA_integer_, NA_integer_, attr(ll, "df"),
+      NA_integer_, NA_integer_),
+    estimate = c(a[1], s$r.squared, s$adj.r.squared, s$sigma, ll, aic, bic),
+    p.value = c(fp, NA_real_, NA_real_, NA_real_, NA_real_, NA_real_, NA_real_),
     stringsAsFactors = FALSE,
     check.rows = FALSE,
     row.names = NULL,
